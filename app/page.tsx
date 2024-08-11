00f0fb0f46edead0593db8bd4dfa69031b622617
hustlerZzZ <rishabpal2000@gmail.com>
2024-08-11 12:02:40 +0530

    Back Up Display

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col gap-4 justify-center items-center h-screen">
+            <h1 className="font-bold text-2xl lg:text-9xl">
+                Working on New Portfolio...
+            </h1>
+            <h2 className="text-xl lg:text-7xl font-semibold">
+                Soon it will be live...
+            </h2>
+        </div>
+    );
+}
