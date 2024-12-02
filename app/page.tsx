7deb645aa1753040d3ee5a94d19ece0db2870475
hustlerZzZ <rishabpal2000@gmail.com>
2024-12-02 17:31:01 +0530

    Fixed a minor bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { ModeToggle } from "@/components/ui/mode-toggle";
-
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-full">
       {/*<ModeToggle />*/}
-      <h1 className="text-4xl">Working on new Portfolio</h1>
+      <h1 className="text-4xl">Working on new Portfolio 🎉🎉</h1>
     </div>
   );
 }
