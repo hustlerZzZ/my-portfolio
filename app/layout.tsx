2463430f241eb409d404809bb4fb5561641eb369
hustlerZzZ <rishabpal2000@gmail.com>
2024-12-02 17:28:39 +0530

    Working on new portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Josefin_Sans } from "next/font/google";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-    title: "Portfolio Live",
-    description: "Portfolio made with ❤️ by Prince Pal.",
+  title: "Prince Portfolio / Full Stack Developer",
+  description:
+    "Prince Pal-Experienced Full-Stack Developer skilled in HTML, CSS, JavaScript, React.js, Node.js, and MongoDB. Crafting responsive UIs, robust backends, and optimized web solutions. Explore my portfolio to discover innovative projects in e-commerce, DevOps, and modern web development.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${josefin.className} antialiased h-screen`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
